8ab846a2d6a4b1274069e4ccd345177118e07851
Carlos Enrique López Garcés <carlos.lopez.garces@gmail.com>
2016-04-03 13:03:12 -0500

    Initial Selectors code before rebase on top of docs.

--- src/ga/ga_selectors.rs
+++ src/ga/ga_selectors.rs
@@ -6,12 +6,37 @@ use super::ga_core::GASolution;
 /// Interface to Selection Schemes
 pub trait GASelector<T: GASolution>
 {
-    /// Assign the population on which to operate
-    fn assign(&mut self, population :Vec<&T>);
-    /// Update internal state
+    fn assign(&mut self, population: GAPopulation<T>);
+
     fn update(&mut self);
-    /// Return an individual from the population
+
     fn select(&mut self) -> &T;
 }
 
-// TODO: Selectors
+// Do we need {RAW, SCALED} enum? Why not just boolean?
+
+struct GARankSelector<T: GASolution>
+{
+    // Q: How to make this member common to all GASelectors?
+    population: GAPopulation<T>,
+}
+
+impl GASelector<T: GASolution> for GARankSelector<T>
+{
+    // GASelect gets ownership of population.
+    fn assign(&mut self, population: GAPopulation<T>)
+    {
+        self.population = population;
+    }
+
+    // GARankSelector implements an empty update().
+    fn update(&mut self)
+    {
+
+    }
+
+    fn select(&mut self) -> &T
+    {
+
+    }
+}
