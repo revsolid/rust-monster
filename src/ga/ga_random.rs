3007fa7153eb09eba38ca25202b3a6ffa657e79d
Carlos Enrique López Garcés <carlos.lopez.garces@gmail.com>
2016-04-08 13:14:50 -0500

    Add ga_random_range().

--- src/ga/ga_random.rs
+++ src/ga/ga_random.rs
@@ -5,10 +5,18 @@
 ///
 /// Wrapper around the rand crate
 ///
-use rand::random;
+use rand::{random, Rng, thread_rng};
+use rand::distributions::range::SampleRange;
+
 pub fn ga_random_float() -> f32
 {
     random()
+}
+
+// Return a random number from range [low, high).
+pub fn ga_random_range<T: PartialOrd + SampleRange>(low: T, high: T) -> T
+{
+    thread_rng().gen_range(low, high)
 }
 
 pub fn ga_random_float_test(v: f32) -> bool
